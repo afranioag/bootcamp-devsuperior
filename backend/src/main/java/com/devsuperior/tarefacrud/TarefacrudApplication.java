5d00ddfdf4b3b5569f5671a5d3b76f3ab2d777a5
afranio.garcia <afranio.cet@gmail.com>
2021-11-11 22:47:20 -0300

    project created

---
+++ backend/src/main/java/com/devsuperior/tarefacrud/TarefacrudApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.tarefacrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarefacrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarefacrudApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/tarefacrud/TarefacrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.tarefacrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarefacrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
