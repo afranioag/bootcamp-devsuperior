37599dec12b28795c78bf43c2b3ae7fa15377505
afranio.garcia <afranio.cet@gmail.com>
2021-12-18 17:35:37 -0300

    exercicio bds4

--- 03_validacao_seguranca/bds04-main/src/main/java/com/devsuperior/bds04/Bds04Application.java
+++ 03_validacao_seguranca/bds04-main/src/main/java/com/devsuperior/bds04/Bds04Application.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Bds04Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Bds04Application.class, args);
 	}
-
 }
---
+++ 03_validacao_seguranca/bds04-main/src/main/java/com/devsuperior/bds04/services/EventService.java
@@ -0,0 +1,45 @@
+package com.devsuperior.bds04.services;
+
+import com.devsuperior.bds04.dto.EventDTO;
+import com.devsuperior.bds04.entities.City;
+import com.devsuperior.bds04.entities.Event;
+import com.devsuperior.bds04.repositories.CityRepository;
+import com.devsuperior.bds04.repositories.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class EventService {
+
+    @Autowired
+    private EventRepository repository;
+
+    @Autowired
+    private CityRepository cityRepository;
+
+    @Transactional(readOnly = true)
+    public Page<EventDTO> findAll(Pageable pageable) {
+        Page<Event> page = repository.findAll(pageable);
+        return page.map(x -> new EventDTO(x));
+    }
+
+    @Transactional
+    public EventDTO insert(EventDTO dto) {
+
+        Event entity = new Event();
+        City city = cityRepository.findById(dto.getCityId()).get();
+
+        entity.setName(dto.getName());
+        entity.setDate(dto.getDate());
+        entity.setDate(dto.getDate());
+        entity.setUrl(dto.getUrl());
+        entity.setCity(city);
+
+        entity = repository.save(entity);
+
+        return new EventDTO(entity);
+    }
+}
