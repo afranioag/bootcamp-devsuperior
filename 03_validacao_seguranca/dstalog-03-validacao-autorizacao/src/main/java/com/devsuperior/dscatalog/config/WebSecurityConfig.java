169dd0c84dc12730c9396544316ec215f7d6699a
afranio.garcia <afranio.cet@gmail.com>
2021-12-18 02:01:33 -0300

    add information token

--- 03_validacao_seguranca/dstalog-03-validacao-autorizacao/src/main/java/com/devsuperior/dscatalog/config/WebSecurityConfig.java
+++ 03_validacao_seguranca/dstalog-03-validacao-autorizacao/src/main/java/com/devsuperior/dscatalog/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dscatalog.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
@@ -24,6 +25,9 @@ import java.util.Arrays;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
 
     @Autowired
     private Environment env;
@@ -77,7 +81,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public JwtAccessTokenConverter accessTokenConverter() {
         JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-        tokenConverter.setSigningKey("MY-JWT-SECRET");
+        tokenConverter.setSigningKey(jwtSecret);
         return tokenConverter;
     }
 
