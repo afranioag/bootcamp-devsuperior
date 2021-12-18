d99ada2ecddc996b0f43b1292c7870e11b0479c2
afranio.garcia <afranio.cet@gmail.com>
2021-12-18 14:40:21 -0300

    projeto bds4 para desafio

--- 02_testes-automatizados/dstalog-02-teste-automatizados/src/main/java/com/devsuperior/dscatalog/services/exceptions/ResourceEntityNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.devsuperior.dscatalog.services.exceptions;
-
-public class ResourceEntityNotFoundException extends RuntimeException {
-    private static final long seriaLVersionUID = 1L;
-
-    public ResourceEntityNotFoundException(String msg) {
-        super(msg);
-    }
-}
--- 03_validacao_seguranca/dstalog-03-validacao-autorizacao/src/main/java/com/devsuperior/dscatalog/services/exceptions/DataBaseException.java
+++
@@ -1,9 +0,0 @@
-package com.devsuperior.dscatalog.services.exceptions;
-
-public class DataBaseException extends RuntimeException {
-    private static final long seriaLVersionUID = 1L;
-
-    public DataBaseException(String msg) {
-        super(msg);
-    }
-}
